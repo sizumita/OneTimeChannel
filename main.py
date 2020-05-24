17e6ebef0e61408aae6e5d6df3f46478f5067692
Sumito Izumita <sumito@izumita.com>
2020-05-24 22:01:55 +0900

    cogの読み込みが間違っていたのを直した

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 bot = MyBot()
 
-# extensions = ['cogs.operation', 'cogs.usually', 'cogs.watch', 'cogs.join_channel']
-extensions = ['cogs.jinrou']
+extensions = ['cogs.operation', 'cogs.usually', 'cogs.watch', 'cogs.join_channel']
+# extensions = ['cogs.jinrou']
 
 for extension in extensions:
     bot.load_extension(extension)
